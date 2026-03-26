e34cfabb620c8e93646ceb529b5c572e8e4a3a90
João Luis <joaoluislopesaraujo088@gmail.com>
2026-03-26 20:45:57 -0300

    Initial commit

---
+++ src/br/com/ifba/atividade03/CalculadoraIdade.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade03;
+
+/**
+ * Classe para calcular idade
+ * @author joaol
+ */
+public class CalculadoraIdade {
+    
+    public int calcularIdade(int anoNascimento){
+        int anoAtual = 2025;
+        return anoAtual - anoNascimento;
+    }
+}
